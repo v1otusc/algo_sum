0824d0f3f70423967b609a34a7d3911424bd220f
v1outsc <v1otusc@yeah.net>
2021-04-14 22:30:14 +0800

    finished binary tree

--- [13] 树/二叉查找树/leetcode-173-Binary-Search-Tree-Iterator.cpp
+++ [13] 树/二叉查找树/leetcode-173-Binary-Search-Tree-Iterator.cpp
@@ -1,7 +1,14 @@
 /*
  * @Description:
  *
- * 
+ * 实现一个二叉搜索树迭代器类BSTIterator
+ * ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ *
+ * - BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点
+ *   root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST
+ *   中的数字，且该数字小于 BST 中的任何元素。
+ * - boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false
+ * - int next()将指针向右移动，然后返回指针处的数字。
  *
  * @输入示例：
  *
@@ -17,7 +24,11 @@
  *
  * @限制条件：
  *
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回
+ * BST 中的最小元素。
  *
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST
+ * 的中序遍历中至少存在一个下一个数字。
  *
  * @题目思路：
  *
@@ -30,11 +41,44 @@
 
 #include "bits/stdc++.h"
 
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
 using namespace std;
 
-class Solution {
+class BSTIterator {
+ private:
+  void inorder(TreeNode* root, vector<int>& res) {
+    if (!root) {
+      return;
+    }
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
+  }
+  
+  vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    inorder(root, res);
+    return res;
+  }
+
+  vector<int> arr;
+  int idx;
+
  public:
-  int solution(vector<int>& g, vector<int>& s) {}
+  BSTIterator(TreeNode* root) : idx(0), arr(inorderTraversal(root)) {}
+
+  int next() { return arr[idx++]; }
+
+  bool hasNext() { return (idx < arr.size()); }
 };
 
 int main(int argc, char const* argv[]) { return 0; }
--- [13] 树/二叉查找树/leetcode-99-recovery-binary-search-tree.cpp
+++ [13] 树/二叉查找树/leetcode-99-recovery-binary-search-tree.cpp
@@ -13,7 +13,7 @@
  *
  * @输入输出说明：
  *
- *
+ * 输入是一个被误交换两个节点的二叉查找树,输出是改正后的二叉查找树。
  *
  * @限制条件：
  *
@@ -21,7 +21,13 @@
  *
  * @题目思路：
  *
+ * 使用中序遍历这个二叉查找树,同时设置一个 prev
+ * 指针,记录当前节点中序遍历时的前节点，如果当前节点大于 prev
+ * 节点的值,说明需要调整次序。
+ * 有一个技巧是如果遍历整个序列过程中只出现了一次次序错误,说明就是这两个相邻节点进行了交换
+ * 如果出现了两次次序错误，那就需要交换这两个节点，两个节点不相邻。
  *
+ * 一定要记住，对 BST 进行中序遍历的结果即为排好序的数组
  *
  * @LastEditors: zzh
  * @LastEditTime: 2021-01-14 15:07:57
@@ -32,9 +38,61 @@
 
 using namespace std;
 
-class Solution {
- public:
-  int solution(vector<int>& g, vector<int>& s) {}
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
-int main(int argc, char const* argv[]) { return 0; }
+class Solution {
+ public:
+  void recoverTree(TreeNode *root) {
+    vector<int> nums;
+    inorder(root, nums);
+    pair<int, int> swapped = findTwoSwapped(nums);
+    recover(root, 2, swapped.first, swapped.second);
+  }
+
+  void inorder(TreeNode *root, vector<int> &nums) {
+    if (root == nullptr) {
+      return;
+    }
+    inorder(root->left, nums);
+    nums.push_back(root->val);
+    inorder(root->right, nums);
+  }
+
+  pair<int, int> findTwoSwapped(vector<int> &nums) {
+    int x = -1;
+    int y = -1;
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      // 发生了错误
+      if (nums[i + 1] < nums[i]) {
+        y = nums[i + 1];
+        if (x == -1) {
+          x = nums[i];
+        } else
+          break;
+      }
+    }
+    pair<int, int> res{x, y};
+    return res;
+  }
+
+  void recover(TreeNode *root, int count, int x, int y) {
+    if (root != nullptr) {
+      if (root->val == x || root->val == y) {
+        root->val = (root->val == x) ? y : x;
+        if (--count == 0) return;
+      }
+      recover(root->left, count, x, y);
+      recover(root->right, count, x, y);
+    }
+  }
+};
+
+int main(int argc, char const *argv[]) { return 0; }
