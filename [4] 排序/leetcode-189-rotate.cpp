17d8e82e474f7ffff468a3fc54d6698a7557b204
v1otusc <v1otusc@yeah.net>
2021-09-04 22:30:34 +0800

    update

---
+++ [4] 排序/leetcode-189-rotate.cpp
@@ -0,0 +1,45 @@
+/*
+ * @Description:
+ *
+ *
+ *
+ * @输入示例：
+ *
+ *
+ *
+ * @输出实例：
+ *
+ *
+ *
+ * @输入输出说明：
+ *
+ *
+ *
+ * @限制条件：
+ *
+ *
+ *
+ * @题目思路：
+ *
+ * 原地算法，比较巧妙
+ *
+ * @LastEditors: zzh
+ * @LastEditTime: 2021-01-14 15:07:57
+ * @FilePath: /algo_sum/template.cpp
+ */
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+ public:
+  void rotate(vector<int>& nums, int k) {
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+  }
+};
+
+int main(int argc, char const* argv[]) { return 0; }
