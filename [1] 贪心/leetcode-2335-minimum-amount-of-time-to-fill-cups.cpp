8ff5887051ed9fabfb6505a13f93d571c3e5cbeb
v1otusc <v1otusc@yeah.net>
2023-02-11 23:10:44 +0800

    update greedy

---
+++ [1] 贪心/leetcode-2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,45 @@
+/*
+ * @Description:
+ *
+ * 
+ *
+ * @输入示例：
+ *
+ *
+ *
+ * @输出实例：
+ *
+ *
+ *
+ * @输入输出说明：
+ *
+ *
+ *
+ * @限制条件：
+ *
+ *
+ *
+ * @题目思路：
+ *
+ *
+ *
+ * @LastEditors: zzh
+ */
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        sort(amount.begin(), amount.end());
+        if (amount[2] > amount[1] + amount[0]) {
+            return amount[2];
+        }
+        return (accumulate(amount.begin(), amount.end(), 0) + 1) / 2;
+    }
+};
+
+
+int main(int argc, char const* argv[]) { return 0; }
