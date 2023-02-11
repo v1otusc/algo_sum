ca9799cbf687f14b85e124648912d308ad2a1df5
v1otusc <v1otusc@yeah.net>
2023-02-11 23:16:03 +0800

    update greedy

--- [1] 贪心/leetcode-2335-minimum-amount-of-time-to-fill-cups.cpp
+++ [1] 贪心/leetcode-2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -31,15 +31,15 @@
 using namespace std;
 
 class Solution {
-public:
-    int fillCups(vector<int>& amount) {
-        sort(amount.begin(), amount.end());
-        if (amount[2] > amount[1] + amount[0]) {
-            return amount[2];
-        }
-        return (accumulate(amount.begin(), amount.end(), 0) + 1) / 2;
+ public:
+  int fillCups(vector<int>& amount) {
+    sort(amount.begin(), amount.end());
+    if (amount[2] > amount[1] + amount[0]) {
+      return amount[2];
     }
+    // 地板除，舍去
+    return (accumulate(amount.begin(), amount.end(), 0) + 1) / 2;
+  }
 };
-
 
 int main(int argc, char const* argv[]) { return 0; }
